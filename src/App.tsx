f048c2f034fd32878e70e80f1fe5e3d16dc85885
David Paul Graham <dpgraham4401@gmail.com>
2023-12-15 17:39:59 -0500

    remove edges on click (currently cannot re-expand) but this is work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,62 @@
-import {useCallback} from 'react';
-import ReactFlow, {useNodesState, useEdgesState, addEdge, Node, MiniMap, Controls, Edge, Connection} from 'reactflow';
+import React, {useCallback} from 'react';
+import ReactFlow, {
+    addEdge,
+    Background,
+    BackgroundVariant,
+    Connection,
+    Controls,
+    Edge,
+    MiniMap,
+    Node,
+    useEdgesState,
+    useNodesState
+} from 'reactflow';
 
 import 'reactflow/dist/style.css';
 
 interface ManifestNode extends Node {
+    expanded?: boolean;
 }
 
 const initialNodes: Array<ManifestNode> = [
-    {id: '1', connectable: false, draggable: false, position: {x: 100, y: 100}, data: {label: 'foo'}},
+    {id: '1', expanded: false, connectable: false, draggable: false, position: {x: 100, y: 100}, data: {label: 'foo'}},
     {id: '2', hidden: true, connectable: false, draggable: false, position: {x: 100, y: 200}, data: {label: 'bar'}},
     {id: '3', hidden: true, connectable: false, draggable: false, position: {x: 300, y: 200}, data: {label: 'xxx'}},
 ];
 const initialEdges: Array<Edge> = [{id: 'e1-2', source: '1', target: '2'}, {id: 'e1-3', source: '1', target: '3'}];
 
 const getNodeTargets = ({edges, source}: { edges: Array<Edge>, source: string }): Array<string> => {
-    const targets = edges.filter(edge => edge.source === source).map(edge => edge.target)
-    console.log(targets)
-    return targets
+    return edges.filter(edge => edge.source === source).map(edge => edge.target)
 }
 
-const setHiddenFalse = ({targets, nodes}: { targets: Array<string>, nodes: Array<Node> }): Array<Node> => {
+const getEdges = ({edges, source}: { edges: Array<Edge>, source: string }): Array<Edge> => {
+    return edges.filter(edge => edge.source !== source)
+}
+
+const setHiddenNodes = ({targets, nodes, expanded}: {
+    targets: Array<string>,
+    nodes: Array<ManifestNode>
+    expanded: boolean
+}): Array<ManifestNode> => {
     return nodes.map(node => {
         if (targets.includes(node.id)) {
-            return {...node, hidden: false}
+            return {...node, hidden: !expanded}
         }
         return node
     })
+}
 
+const setExpanded = ({source, nodes}: { source: string, nodes: Array<ManifestNode> }): Array<ManifestNode> => {
+    return nodes.map(node => {
+        if (node.id === source) {
+            return {...node, expanded: !node.expanded}
+        }
+        return node
+    })
 }
 
 export default function App() {
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [nodes, setNodes] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
     const onConnect = useCallback(
@@ -38,11 +64,19 @@ export default function App() {
         [setEdges],
     );
 
-    const onClick = (event: any, node: any) => {
-        const targets = getNodeTargets({edges, source: node.id})
-        const newNodes = setHiddenFalse({nodes, targets})
-        setNodes(newNodes)
-    }
+    const onClick = useCallback(
+        (event: React.MouseEvent, node: ManifestNode) => {
+            console.log('click', node)
+            const targets = getNodeTargets({edges, source: node.id})
+            const intermediaNodes = setExpanded({source: node.id, nodes})
+            const newNodes = setHiddenNodes({nodes: intermediaNodes, targets, expanded: !node.expanded})
+            if (node.expanded) {
+                setEdges(getEdges({edges, source: node.id}))
+            }
+            setNodes(newNodes)
+        },
+        [edges, nodes, setNodes]
+    )
 
     return (
         <div style={{width: '100vw', height: '100vh'}}>
@@ -53,6 +87,7 @@ export default function App() {
                 onConnect={onConnect}
                 onNodeClick={onClick}
             >
+                <Background variant={BackgroundVariant.Dots}/>
                 <MiniMap nodeStrokeWidth={3}/>
                 <Controls/>
             </ReactFlow>
