e70844ea6af467d0f5b04b040b623696dae240f1
David Paul Graham <dpgraham4401@gmail.com>
2023-12-16 13:53:29 -0500

    working nested layers

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,19 @@ const initialNodes: Array<ManifestNode> = [
     {id: '1', expanded: false, connectable: false, draggable: false, position: {x: 100, y: 100}, data: {label: 'foo'}},
     {id: '2', hidden: true, connectable: false, draggable: false, position: {x: 100, y: 200}, data: {label: 'bar'}},
     {id: '3', hidden: true, connectable: false, draggable: false, position: {x: 300, y: 200}, data: {label: 'xxx'}},
+    {id: '4', hidden: true, connectable: false, draggable: false, position: {x: 200, y: 300}, data: {label: 'bar'}},
+    {id: '5', hidden: true, connectable: false, draggable: false, position: {x: 400, y: 300}, data: {label: 'xxx'}},
+    {id: '6', hidden: true, connectable: false, draggable: false, position: {x: 100, y: 300}, data: {label: 'bar'}},
+    {id: '7', hidden: true, connectable: false, draggable: false, position: {x: 300, y: 300}, data: {label: 'xxx'}},
 ];
-const initialEdges: Array<Edge> = [{id: 'e1-2', hidden: true, source: '1', target: '2'}, {
-    id: 'e1-3',
-    hidden: true,
-    source: '1',
-    target: '3'
-}];
+const initialEdges: Array<Edge> = [
+    {id: 'e1-2', hidden: true, source: '1', target: '2'},
+    {id: 'e1-3', hidden: true, source: '1', target: '3'},
+    {id: 'e3-4', hidden: true, source: '3', target: '4'},
+    {id: 'e3-5', hidden: true, source: '3', target: '5'},
+    {id: 'e2-6', hidden: true, source: '2', target: '6'},
+    {id: 'e2-7', hidden: true, source: '2', target: '7'},
+];
 
 const getNodeTargets = ({edges, source}: { edges: Array<Edge>, source: string }): Array<string> => {
     return edges.filter(edge => edge.source === source).map(edge => edge.target)
