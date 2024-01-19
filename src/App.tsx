d1f8d2e6d81d6bab17e02b3eeb910f3d25796022
David Graham <dpgraham4401@gmail.com>
2024-01-19 16:13:36 -0500

    add DAGre library dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Tree } from "components/Tree/Tree";
 
 import "reactflow/dist/style.css";
-import { Config } from "services/config/configService";
 
 export default function App() {
-  Config.fetchSubmitManifestTree();
   return <Tree />;
 }
--- src/services/config/configService.ts
+++ src/services/config/configService.ts
@@ -3,7 +3,6 @@
 export const Config = {
   /** read and return the decision tree from a json file */
   fetchSubmitManifestTree: async () => {
-    const foo = await fetch("/submit-manifest.json");
-    return foo;
+    return await fetch("/submit-manifest.json");
   },
 };
