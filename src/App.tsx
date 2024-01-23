f113e1027ba1d51b1a6230a890cdc7a0a8b45eeb
David Graham <dpgraham4401@gmail.com>
2024-01-22 20:44:11 -0500

    hook up zustand to redux dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Tree } from 'components/Tree/Tree';
 
+import useTreeStore from 'store/treeStore';
+
 export default function App() {
+  const nodes = useTreeStore((state) => state.nodes);
+  console.log(nodes);
   return <Tree />;
 }
--- src/store/treeStore.ts
+++ src/store/treeStore.ts
@@ -11,9 +11,13 @@ import {
   applyEdgeChanges,
   EdgeChange,
 } from 'reactflow';
+import { ManifestTree } from 'services/tree/treeService';
+import type {} from '@redux-devtools/extension';
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 export type TreeStore = {
+  tree: ManifestTree;
   nodes: Node[];
   edges: Edge[];
   onNodesChange: OnNodesChange;
@@ -21,24 +25,36 @@ export type TreeStore = {
   onConnect: OnConnect;
 };
 
-const treeStore = create<TreeStore>((set, get) => ({
-  nodes: [],
-  edges: [],
-  onNodesChange: (changes: NodeChange[]) => {
-    set({
-      nodes: applyNodeChanges(changes, get().nodes),
-    });
-  },
-  onEdgesChange: (changes: EdgeChange[]) => {
-    set({
-      edges: applyEdgeChanges(changes, get().edges),
-    });
-  },
-  onConnect: (connection: Connection) => {
-    set({
-      edges: addEdge(connection, get().edges),
-    });
-  },
-}));
+const treeStore = create<TreeStore>()(
+  devtools(
+    (set, get) => ({
+      tree: {},
+      nodes: [],
+      edges: [],
+      onNodesChange: (changes: NodeChange[]) => {
+        set({
+          nodes: applyNodeChanges(changes, get().nodes),
+        });
+      },
+      onEdgesChange: (changes: EdgeChange[]) => {
+        set({
+          edges: applyEdgeChanges(changes, get().edges),
+        });
+      },
+      onConnect: (connection: Connection) => {
+        set({
+          edges: addEdge(connection, get().edges),
+        });
+      },
+      showNode: (nodeId: string): void => {
+        const node = get().tree[nodeId];
+        if (node) {
+          console.log(node);
+        }
+      },
+    }),
+    { name: 'decisionTree', anonymousActionType: 'unknown' }
+  )
+);
 
 export default treeStore;
