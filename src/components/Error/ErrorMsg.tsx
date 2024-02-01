b03995856f62eb4873d79941b092443a27850119
David Paul Graham <dpgraham4401@gmail.com>
2024-02-01 09:17:47 -0500

    add unit test for Error components

--- src/components/Error/ErrorMsg.tsx
+++ src/components/Error/ErrorMsg.tsx
@@ -1,12 +1,14 @@
-import styles from 'components/Error/errorMsg.module.css';
+import styles from './errorMsg.module.css';
 
+/**
+ * ErrorMsg displays a simple error message and a link to file a ticket.
+ */
 export const ErrorMsg = () => {
   return (
     <div className={styles.center}>
       <div className={styles.errorBox}>
-        <h1>Found a bug?</h1>
+        <h1>Something went wrong</h1>
         <p>
-          Tell us about it,{' '}
           <a href={import.meta.env.VITE_ISSUE_URL ?? '#'} className={styles.errorLink}>
             file a ticket
           </a>
