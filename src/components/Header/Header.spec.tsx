e8fe26b25c4e204367221ba793432bbd46154b3e
David Paul Graham <dpgraham4401@gmail.com>
2024-02-06 13:18:36 -0500

    header shows a button that will allow users to toggle the layout

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -8,7 +8,11 @@ afterEach(() => {});
 describe('Header', () => {
   it('renders a title', () => {
     const title = 'hello';
-    render(<Header treeTitle={title} />);
+    render(<Header treeTitle={title} direction={'TB'} setDirection={() => undefined} />);
     expect(screen.getByText(title)).toBeInTheDocument();
+  });
+  it('renders a button to toggle the layout', () => {
+    render(<Header treeTitle={'foo'} direction={'TB'} setDirection={() => undefined} />);
+    expect(screen.getByRole('button', { name: /layout/i })).toBeInTheDocument();
   });
 });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,32 @@
+import { DagDirection } from 'store/DagSlice/dagSlice';
 import styles from './header.module.css';
 
 interface HeaderProps {
   treeTitle: string;
+  direction: DagDirection;
+  setDirection: (direction: DagDirection) => void;
 }
 
-export const Header = ({ treeTitle }: HeaderProps) => {
+export const Header = ({ treeTitle, setDirection, direction }: HeaderProps) => {
+  const isHorizontal = direction === 'LR';
+
+  const toggleDirection = () => {
+    setDirection(direction === 'TB' ? 'LR' : 'TB');
+  };
+
   return (
     <>
       <div className={styles.treeHeader}>
         <div className={styles.headerBanner}>
           <h1>{treeTitle}</h1>
+          <div>
+            <button
+              aria-label={`use ${isHorizontal ? 'vertical' : 'horizontal'} layout`}
+              onClick={toggleDirection}
+            >
+              T
+            </button>
+          </div>
         </div>
       </div>
     </>
