68322f4b9392448b05e0cf65e3122c083a932d19
David Paul Graham <dpgraham4401@gmail.com>
2024-02-06 12:45:52 -0500

    update buildPositionedTree to accept optional direction argument that changes the DAG rank direction

--- src/store/DagSlice/layout.spec.ts
+++ src/store/DagSlice/layout.spec.ts
@@ -1,10 +1,10 @@
 import '@testing-library/jest-dom';
 import { PositionUnawareDecisionTree } from 'store/DagSlice/dagSlice';
 import { buildPositionedTree } from 'store/DagSlice/layout';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe('DAG layout', () => {
-  test('accepts a position unaware tree and returns position aware tree', () => {
+  it('accepts a position unaware tree and returns position aware tree', () => {
     const nodeId = '2';
     const positionUnawareTree: PositionUnawareDecisionTree = {
       [nodeId]: {
@@ -20,5 +20,30 @@ describe('DAG layout', () => {
     expect('position' in positionUnawareTree[nodeId]).toBe(false);
     expect(typeof tree).toBe('object');
     expect(tree[nodeId].position).toBeDefined();
+  });
+  it('changes rank direction based on optional argument', () => {
+    const parentId = '2';
+    const childId = '3';
+    const positionUnawareTree: PositionUnawareDecisionTree = {
+      [parentId]: {
+        id: parentId,
+        data: {
+          children: [childId],
+          label: 'parent',
+        },
+      },
+      [childId]: {
+        id: childId,
+        data: {
+          children: [],
+          label: 'child',
+        },
+      },
+    };
+    const direction = 'LR';
+    const horizontalTree = buildPositionedTree(positionUnawareTree, direction);
+    const verticalTree = buildPositionedTree(positionUnawareTree);
+    expect(horizontalTree[parentId].position.x).toBeLessThan(horizontalTree[childId].position.x);
+    expect(verticalTree[parentId].position.y).toBeLessThan(verticalTree[childId].position.y);
   });
 });
