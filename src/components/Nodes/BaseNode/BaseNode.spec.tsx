18e757d550e3eb1f2cc1907f3682753934d73c12
David Paul Graham <dpgraham4401@gmail.com>
2024-02-07 19:50:27 -0500

    add test showing node handles handle change of layout in the BaseNode component

--- src/components/Nodes/BaseNode/BaseNode.spec.tsx
+++ src/components/Nodes/BaseNode/BaseNode.spec.tsx
@@ -1,28 +1,45 @@
 import '@testing-library/jest-dom';
-import { cleanup, render, screen } from '@testing-library/react';
+import { act, cleanup, render, screen } from '@testing-library/react';
 import { BaseNode } from 'components/Nodes/BaseNode/BaseNode';
 import { ReactFlowProvider } from 'reactflow';
+import useStore from 'store';
 import { afterEach, describe, expect, it } from 'vitest';
+
+const TestComponent = () => {
+  return (
+    <ReactFlowProvider>
+      <BaseNode
+        id={'1'}
+        dragging={false}
+        selected={false}
+        type={''}
+        zIndex={0}
+        isConnectable={false}
+        xPos={0}
+        yPos={0}
+      >
+        <div>Test</div>
+      </BaseNode>
+    </ReactFlowProvider>
+  );
+};
 
 describe('BaseNode', () => {
   afterEach(() => cleanup());
   it('renders', () => {
-    render(
-      <ReactFlowProvider>
-        <BaseNode
-          id={'1'}
-          dragging={false}
-          selected={false}
-          type={''}
-          zIndex={0}
-          isConnectable={false}
-          xPos={0}
-          yPos={0}
-        >
-          <div>Test</div>
-        </BaseNode>
-      </ReactFlowProvider>
-    );
+    render(<TestComponent />);
     expect(screen.getByTestId('node-1')).toBeInTheDocument();
+  });
+  it('handles changes in tree layout', () => {
+    useStore.setState({ dagDirection: 'LR' });
+    const { rerender } = render(<TestComponent />);
+    expect(screen.getByTestId('left-handle')).toBeInTheDocument();
+    expect(screen.getByTestId('right-handle')).toBeInTheDocument();
+    act(() => {
+      useStore.setState({ dagDirection: 'TB' });
+    });
+    rerender(<TestComponent />);
+    expect(screen.getByTestId('top-handle')).toBeInTheDocument();
+    expect(screen.getByTestId('bottom-handle')).toBeInTheDocument();
   });
 });
