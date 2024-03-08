2940b3b3d6e36cfa5eaece432be293fac7366c91
David Paul Graham <dpgraham4401@gmail.com>
2024-03-08 16:44:30 -0500

    unit tests for decisionUtils

--- src/store/DecisionSlice/decisionUtils.spec.ts
+++ src/store/DecisionSlice/decisionUtils.spec.ts
@@ -1,6 +1,13 @@
 import '@testing-library/jest-dom';
 import { DecisionTree } from 'store/DagNodeSlice/dagNodeSlice';
-import { getDescendantIds, getSiblingIds } from 'store/DecisionSlice/decisionUtils';
+import {
+  getDescendantIds,
+  getSiblingIds,
+  setCollapsed,
+  setExpanded,
+  setNodesHidden,
+  setNodeVisible,
+} from 'store/DecisionSlice/decisionUtils';
 import { describe, expect, suite, test } from 'vitest';
 
 suite('Tree Slice internals', () => {
@@ -54,6 +61,53 @@ suite('Tree Slice internals', () => {
       expect(getSiblingIds(tree, siblingId3)).toEqual([siblingId2, siblingId4]);
       expect(getSiblingIds(tree, siblingId4)).toEqual([siblingId2, siblingId3]);
     });
-    describe('');
+  });
+  describe('Set Node Visible', () => {
+    test('Set hidden to false (idempotent)', () => {
+      const tree: DecisionTree = {
+        // @ts-expect-error - use a minimal tree for testing
+        '1': { id: '1', hidden: true, data: { label: 'foo' } },
+      };
+      const updatedTree = setNodeVisible(tree, ['1']);
+      expect(updatedTree['1'].hidden).toBe(false);
+      const idempotentTree = setNodeVisible(updatedTree, ['1']);
+      expect(idempotentTree['1'].hidden).toBe(false);
+    });
+  });
+  describe('Set Node Hidden', () => {
+    test('Set hidden to true (idempotent)', () => {
+      const tree: DecisionTree = {
+        // @ts-expect-error - use a minimal tree for testing
+        '1': { id: '1', hidden: false, data: { label: 'foo' } },
+      };
+      const updatedTree = setNodesHidden(tree, ['1']);
+      expect(updatedTree['1'].hidden).toBe(true);
+      const idempotentTree = setNodesHidden(updatedTree, ['1']);
+      expect(idempotentTree['1'].hidden).toBe(true);
+    });
+  });
+  describe('Set Node Expanded', () => {
+    test('Set expanded to true (idempotent)', () => {
+      const tree: DecisionTree = {
+        // @ts-expect-error - use a minimal tree for testing
+        '1': { id: '1', hidden: false, data: { label: 'foo', expanded: false } },
+      };
+      const updatedTree = setExpanded(tree, ['1']);
+      expect(updatedTree['1'].data.expanded).toBe(true);
+      const idempotentTree = setExpanded(updatedTree, ['1']);
+      expect(idempotentTree['1'].data.expanded).toBe(true);
+    });
+  });
+  describe('Set Node Collapsed', () => {
+    test('Set expanded to false (idempotent)', () => {
+      const tree: DecisionTree = {
+        // @ts-expect-error - use a minimal tree for testing
+        '1': { id: '1', hidden: false, data: { label: 'foo', expanded: true } },
+      };
+      const updatedTree = setCollapsed(tree, ['1']);
+      expect(updatedTree['1'].data.expanded).toBe(false);
+      const idempotentTree = setCollapsed(updatedTree, ['1']);
+      expect(idempotentTree['1'].data.expanded).toBe(false);
+    });
   });
 });
--- src/store/DecisionSlice/decisionUtils.ts
+++ src/store/DecisionSlice/decisionUtils.ts
@@ -28,16 +28,19 @@ export const setNodeVisible = (tree: DecisionTree, nodeIds: string[]) => {
   return tree;
 };
 
+/** set hidden to true */
 export const setNodesHidden = (tree: DecisionTree, nodeIds: string[]) => {
   nodeIds.forEach((id) => (tree[id].hidden = true));
   return tree;
 };
 
+/** set expanded to true */
 export const setExpanded = (tree: DecisionTree, nodeIds: string[]) => {
   nodeIds.forEach((id) => (tree[id].data.expanded = true));
   return tree;
 };
 
+/** set expanded to false */
 export const setCollapsed = (tree: DecisionTree, nodeIds: string[]) => {
   nodeIds.forEach((id) => (tree[id].data.expanded = false));
   return tree;
