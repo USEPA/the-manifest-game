08fbee7fe4cbe510c74f35725f3e6315fb796705
David Paul Graham <dpgraham4401@gmail.com>
2024-03-25 10:35:20 -0400

    HelpContent displays a friendly message if the necessary content is not passed to it

--- src/components/Help/HelpContent/HelpContent.spec.tsx
+++ src/components/Help/HelpContent/HelpContent.spec.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { cleanup, render, screen } from '@testing-library/react';
 import { afterEach, describe, expect, test } from 'vitest';
-import { HelpContent } from './HelpContent';
+import { HelpContent, TextHelp } from './HelpContent';
 
 afterEach(() => cleanup());
 
@@ -9,5 +9,17 @@ describe('HelpContent', () => {
   test('renders', () => {
     render(<HelpContent />);
     expect(screen.getByTestId(/help-content/i)).toBeInTheDocument();
+  });
+  test('returns a friendly message if help prop is empty', () => {
+    render(<HelpContent />);
+    expect(screen.getByText(/unavailable/i, { exact: false })).toBeInTheDocument();
+  });
+  test('accepts an TextHelp object and displays the data as text', () => {
+    const help: TextHelp = {
+      type: 'text',
+      data: 'This is a help message',
+    };
+    render(<HelpContent help={help} />);
+    expect(screen.getByText(help.data)).toBeInTheDocument();
   });
 });
--- src/components/Help/HelpContent/HelpContent.tsx
+++ src/components/Help/HelpContent/HelpContent.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+type HelpContent = TextHelp;
+
+export interface TextHelp {
+  type: 'text';
+  data: string;
+}
+
+interface HelpContentProps {
+  help?: HelpContent;
+}
+
 /**
  * Renders the help content
  * @constructor
  */
-export const HelpContent = () => {
-  return <div data-testid={'help-content'}></div>;
+export const HelpContent = ({ help }: HelpContentProps) => {
+  if (!help) {
+    return (
+      <div data-testid={'help-content'}>
+        <p>Help is unavailable for this node.</p>
+      </div>
+    );
+  }
+  if (help.type === 'text') {
+    return <div data-testid={'help-content'}>{help.data}</div>;
+  }
 };
