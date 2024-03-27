b55bbea3dc17b47010093dec989bdda00f1a6c1d
David Paul Graham <dpgraham4401@gmail.com>
2024-03-27 14:34:32 -0400

    simplify spinner to just use an icon and remove the complex css     remove unused DefaultNode css module

--- src/components/Tree/Nodes/DefaultNode/DefaultNode.tsx
+++ src/components/Tree/Nodes/DefaultNode/DefaultNode.tsx
@@ -1,7 +1,5 @@
 import { HelpIcon } from 'components/Help/HelpIcon/HelpIcon';
 import { BaseNode } from 'components/Tree/Nodes/BaseNode/BaseNode';
-
-import styles from 'components/Tree/Nodes/DefaultNode/default.module.css';
 import { useHelp } from 'hooks/useHelp/useHelp';
 import { MouseEventHandler } from 'react';
 import { NodeProps } from 'reactflow';
@@ -19,11 +17,10 @@ export const DefaultNode = ({ data, ...props }: NodeProps<NodeData>) => {
     <BaseNode {...props}>
       <div
         data-testid={`default-node-${props.id}-content`}
-        // className={`${styles.defaultNodeContent}  rounded-xl ${data.status ? 'bg-teal-700' : ''}`}
         className={`flex min-w-80 flex-col items-center justify-center rounded-xl p-6 text-xl text-white ${data.status ? 'bg-teal-700' : 'bg-gradient-to-b from-sky-700 to-sky-900'} ${data.status === 'focused' ? 'animate-pulse' : ''}`}
       >
         {data.help && (
-          <div className={styles.helpIcon}>
+          <div className="absolute right-3 top-3">
             <HelpIcon onClick={handleHelpClick} />
           </div>
         )}
