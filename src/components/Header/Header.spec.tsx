b132c770aa7da7f9b6d769c7529f8d141b5718cc
David Paul Graham <dpgraham4401@gmail.com>
2024-04-08 17:02:24 -0400

    header renders a feedback link

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { cleanup, render, screen } from '@testing-library/react';
 import { ReactFlowProvider } from 'reactflow';
-import { afterEach, describe, expect, test } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 import { Header } from './Header';
 
 const TestComponent = ({ title }: { title?: string }) => {
@@ -18,5 +18,15 @@ describe('Header', () => {
     const title = 'hello';
     render(<TestComponent title={title} />);
     expect(screen.getByText(title)).toBeInTheDocument();
+  });
+  test('no feedback link rendered if no config', () => {
+    vi.stubEnv('VITE_ISSUE_URL', '');
+    render(<TestComponent />);
+    expect(screen.queryByRole('link', { name: /feedback/i })).not.toBeInTheDocument();
+  });
+  test('feedback link rendered if config present', () => {
+    vi.stubEnv('VITE_ISSUE_URL', 'mailto:foo@epa.gov');
+    render(<TestComponent />);
+    expect(screen.queryByRole('link', { name: /feedback/i })).toBeInTheDocument();
   });
 });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,21 @@ interface HeaderProps {
 }
 
 export const Header = ({ treeTitle }: HeaderProps) => {
+  const issueUrl = import.meta.env.VITE_ISSUE_URL;
+
   return (
     <Panel position="top-left" className="w-3/12">
-      <div className="box-border flex w-full justify-between rounded-xl bg-gradient-to-b from-sky-700 to-sky-900 p-2 align-middle">
-        <h1 className="text-xl font-semibold text-white">{treeTitle}</h1>
+      <div className="box-border w-full rounded-xl bg-gradient-to-b from-sky-700 to-sky-900 p-2 align-middle">
+        <div>
+          <h1 className="text-xl font-semibold text-white">{treeTitle}</h1>
+        </div>
+        {issueUrl && (
+          <div>
+            <a href={issueUrl} className="text-sm underline decoration-1">
+              Feedback/Report and Issue
+            </a>
+          </div>
+        )}
       </div>
     </Panel>
   );
