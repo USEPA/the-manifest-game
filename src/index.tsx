764f55180797ca3531d586d5e70bc88caf06872a
David Graham <dpgraham4401@gmail.com>
2024-05-08 10:30:38 -0400

    add routes to the app, 1 for home ('/') and another that redirects all other routes to home ('/')

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
 import { ErrorBoundary, ErrorMsg } from 'components/Error';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import { ReactFlowProvider } from 'reactflow';
 import App from './App';
 import 'reactflow/dist/style.css';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '*',
+    element: <Navigate to={'/'} replace />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<ErrorMsg />}>
       <ReactFlowProvider>
-        <App />
+        <RouterProvider router={router} />
       </ReactFlowProvider>
     </ErrorBoundary>
   </React.StrictMode>
