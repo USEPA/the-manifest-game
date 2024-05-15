69b621e7aea9f85e326c51cceb2f31362d8cf649
David Graham <dpgraham4401@gmail.com>
2024-05-15 10:56:15 -0400

    add responsive design breakpoints to main header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ export const Header = ({ treeTitle }: HeaderProps) => {
   const issueUrl = import.meta.env.VITE_ISSUE_URL;
 
   return (
-    <Panel position="top-left" className="w-3/12">
+    <Panel position="top-left" className="sm:w-5/12 md:w-4/12 lg:w-3/12">
       <div className="box-border w-full rounded-xl bg-gradient-to-b from-sky-700 to-sky-900 p-2 align-middle">
         <div>
           <h1 className="text-xl font-semibold text-white">{treeTitle}</h1>
