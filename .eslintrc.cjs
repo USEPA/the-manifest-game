638e524e8d21b10e383934c61156d3311399e524
David Graham <dpgraham4401@gmail.com>
2024-06-11 14:05:12 -0400

    update eslint config to use typescript-eslint/recommended-type-checked and detect react version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
         'plugin:react-hooks/recommended',
         'plugin:prettier/recommended',
         'plugin:jsx-a11y/recommended',
@@ -12,10 +12,19 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
     plugins: ['react-refresh', 'react', 'prettier'],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         'react/react-in-jsx-scope': 'off',
         'react/jsx-uses-react': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
 };
