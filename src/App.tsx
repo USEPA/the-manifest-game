da253c3c6e2ecf6e5369b5504430cddee59800b7
David Graham <dpgraham4401@gmail.com>
2024-09-18 15:41:33 -0400

    add getter, setter to useUrl hook for tree url parameter and modify App.tsx to automatically set tree to '0' if no tree is specified

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { OffCanvas } from '@/components/OffCanvas/OffCanvas';
 import { Spinner } from '@/components/Spinner/Spinner';
 import { Tree } from '@/components/Tree/Tree';
 import { useDecisionTree, useFetchConfig, useHelp, useUrl } from '@/hooks';
+import { useEffect } from 'react';
 
 /**
  * App - responsible for rendering the decision tree
@@ -13,9 +14,15 @@ import { useDecisionTree, useFetchConfig, useHelp, useUrl } from '@/hooks';
 export default function App() {
   const title = import.meta.env.VITE_APP_TITLE ?? 'The Manifest Game';
   const { config, isLoading: configIsLoading, error: configError } = useFetchConfig(defaultTree);
-  const { pathParam } = useUrl();
+  const { pathParam, treeParam, setTreeParam } = useUrl();
   const { nodes, edges } = useDecisionTree(config, pathParam);
   const { helpIsOpen, hideHelp } = useHelp();
+
+  useEffect(() => {
+    if (!treeParam) {
+      setTreeParam('0');
+    }
+  }, [treeParam, setTreeParam]);
 
   return (
     <>
