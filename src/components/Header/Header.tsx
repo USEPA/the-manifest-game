5e7f2db55c77664336dbf04d05227798823c156f
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2025-07-03 14:26:50 -0400

    Add demo banner (#206)
    * add smaller widget for showing that the deployment to github pages is a demo
    
    * add VITE_IS_DEMO environment variable to GitHub action pipeline for depoying demo to github pages
    
    * revert default title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,6 +20,7 @@ interface HeaderProps {
 
 export const Header = ({ treeTitle }: HeaderProps) => {
   const issueUrl = import.meta.env.VITE_ISSUE_URL;
+  const isDemo = import.meta.env.VITE_IS_DEMO === 'true';
   const { showHelp } = useHelp();
 
   const showInstructions: MouseEventHandler = (event) => {
@@ -32,46 +33,53 @@ export const Header = ({ treeTitle }: HeaderProps) => {
   };
 
   return (
-    <Panel position="top-center" className="mx-0 w-full">
-      <div className="mx-2 box-border min-w-80 rounded-xl bg-rcraBlue p-2">
-        <div className="flex items-center justify-between">
-          <h1 className="text-xl font-semibold text-white">{treeTitle}</h1>
-          <div className="">
-            <DropdownMenu modal={false}>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="outline"
-                  className="rounded-full border-none bg-transparent p-2 text-white hover:bg-slate-400"
-                  name="menu"
-                  aria-label="menu"
-                >
-                  <LuMenu />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="mx-4">
-                <DropdownMenuLabel>Help</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={showInstructions}>
-                  <FaQuestionCircle className="mr-2 size-4" />
-                  <span>How to use</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  {issueUrl && (
-                    <>
-                      <FaBug className="mr-2 size-4" />
-                      <div>
-                        <a href={issueUrl} className="text-sm underline decoration-1">
-                          Feedback/Report and Issue
-                        </a>
-                      </div>
-                    </>
-                  )}
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+    <>
+      <Panel position="top-center" className="mx-0 w-full">
+        <div className="mx-2 box-border min-w-80 rounded-xl bg-rcraBlue p-2">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-xl font-semibold text-white">
+                {treeTitle}{' '}
+                {isDemo && <span className="mx-5 rounded-md bg-red-700 p-1 font-bold">Demo</span>}
+              </h1>
+            </div>
+            <div className="">
+              <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    className="rounded-full border-none bg-transparent p-2 text-white hover:bg-slate-400"
+                    name="menu"
+                    aria-label="menu"
+                  >
+                    <LuMenu />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="mx-4">
+                  <DropdownMenuLabel>Help</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={showInstructions}>
+                    <FaQuestionCircle className="mr-2 size-4" />
+                    <span>How to use</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    {issueUrl && (
+                      <>
+                        <FaBug className="mr-2 size-4" />
+                        <div>
+                          <a href={issueUrl} className="text-sm underline decoration-1">
+                            Feedback/Report and Issue
+                          </a>
+                        </div>
+                      </>
+                    )}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
         </div>
-      </div>
-    </Panel>
+      </Panel>
+    </>
   );
 };
